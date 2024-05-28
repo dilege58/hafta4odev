2f2662bfb40d9144ff501b2f1690fbc925a755cd
Nurhayat Dere <nrhyt1746@gmail.com>
2024-05-28 13:10:28 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/hafta4odev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hafta4odev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/dikdortgen.dart
@@ -0,0 +1,10 @@
+
+double AlanHesaplama(double uzun_kenar, double kisa_kenar) {
+return uzun_kenar * kisa_kenar ;
+}
+
+
+void main() {
+double alan = AlanHesaplama(4.76, 9.54);
+print('Dikdörtgenin alanı: $alan');
+}
